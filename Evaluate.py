9dda92a15642eb346798653e3a84e61d1e35496f
Shriyam-Avasthi <avasthishriyam@gmail.com>
2025-09-23 01:19:14 +0530

    Add basic functioning minmax implementation

---
+++ Evaluate.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+from game.go import Board, opponent_color
+from game.ui import UI
+import pygame
+import time
+from group1 import Agent1v1 as Agent1       # <---------CHANGE THIS TO SUITABLE VERSION
+from group2 import Agent2
+from os.path import join
+from argparse import ArgumentParser
+
+from tqdm import tqdm
+
+class Match:
+    def __init__(self, agent_black=None, agent_white=None, gui=True, dir_save=None):
+        self.agent_black = agent_black
+        self.agent_white = agent_white
+
+        self.board = Board(next_color='BLACK')
+
+        gui = gui if agent_black and agent_white else True
+        self.ui = UI() if gui else None
+        self.dir_save = dir_save
+
+        # Metadata
+
+        self.time_elapsed = None
+
+    @property
+    def winner(self):
+        return self.board.winner
+
+    @property
+    def next(self):
+        return self.board.next
+
+    @property
+    def counter_move(self):
+        return self.board.counter_move
+
+    def start(self):
+        if self.ui:
+            self._start_with_ui()
+        else:
+            self._start_without_ui()
+
+    def _start_with_ui(self):
+        """Start the game with GUI."""
+        self.ui.initialize()
+        self.time_elapsed = time.time()
+
+        # First move is fixed on the center of board
+        first_move = (10, 10)
+        self.board.put_stone(first_move, check_legal=False)
+        self.ui.draw(first_move, opponent_color(self.board.next))
+
+        # Take turns to play move
+        while self.board.winner is None:
+            if self.board.next == 'BLACK':
+                point = self.perform_one_move(self.agent_black)
+            else:
+                point = self.perform_one_move(self.agent_white)
+            # print("!!!", self.board.next, self.board.legal_actions)
+            # Check if action is legal
+            if point not in self.board.legal_actions:
+                continue
+            # print("$$$", self.board.next)
+
+            # Apply action
+            prev_legal_actions = self.board.legal_actions.copy()
+            self.board.put_stone(point, check_legal=False)
+            # Remove previous legal actions on board
+            for action in prev_legal_actions:
+                self.ui.remove(action)
+            # Draw new point
+            self.ui.draw(point, opponent_color(self.board.next))
+            # Update new legal actions and any removed groups
+            if self.board.winner:
+                for group in self.board.removed_groups:
+                    for point in group.points:
+                        self.ui.remove(point)
+                if self.board.end_by_no_legal_actions:
+                    print('Game ends early (no legal action is available for %s)' % self.board.next)
+            else:
+                for action in self.board.legal_actions:
+                    self.ui.draw(action, 'BLUE', 8)
+
+        self.time_elapsed = time.time() - self.time_elapsed
+        if self.dir_save:
+            path_file = join(self.dir_save, 'go_' + str(time.time()) + '.jpg')
+            self.ui.save_image(path_file)
+            print('Board image saved in file ' + path_file)
+
+    def _start_without_ui(self):
+        """Start the game without GUI. Only possible when no human is playing."""
+        # First move is fixed on the center of board
+        self.time_elapsed = time.time()
+        first_move = (10, 10)
+        self.board.put_stone(first_move, check_legal=False)
+
+        # Take turns to play move
+        while self.board.winner is None:
+            if self.board.next == 'BLACK':
+                point = self.perform_one_move(self.agent_black)
+            else:
+                point = self.perform_one_move(self.agent_white)
+
+            # Apply action
+            self.board.put_stone(point, check_legal=False)  # Assuming agent always gives legal actions
+
+
+        if self.board.end_by_no_legal_actions:
+            print('Game ends early (no legal action is available for %s)' % self.board.next)
+
+        self.time_elapsed = time.time() - self.time_elapsed
+
+    def perform_one_move(self, agent):
+        if agent:
+            return self._move_by_agent(agent)
+        else:
+            return self._move_by_human()
+
+    def _move_by_agent(self, agent):
+        if self.ui:
+            pygame.time.wait(100)
+            pygame.event.get()
+        return agent.get_action(self.board)
+
+    def _move_by_human(self):
+        while True:
+            pygame.time.wait(100)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1 and self.ui.outline.collidepoint(event.pos):
+                        x = int(round(((event.pos[0] - 5) / 40.0), 0))
+                        y = int(round(((event.pos[1] - 5) / 40.0), 0))
+                        point = (x, y)
+                        stone = self.board.exist_stone(point)
+                        if not stone:
+                            return point
+
+def main():
+
+    SAMPLE_SIZE = 10
+    black_wins = 0
+    white_wins = 0
+    draws = 0
+    for i in tqdm(range(SAMPLE_SIZE), desc="Progress: ", unit="matches"):
+        agent_black = Agent1('BLACK')
+        agent_white = None #Agent2('WHITE')
+
+        match = Match(agent_black=agent_black, agent_white=agent_white, gui=False, dir_save=None)
+
+        match.start()
+
+        if(match.board.end_by_no_legal_actions): draws += 1
+        elif(match.winner == 'WHITE'): white_wins += 1
+        else: black_wins += 1
+        # print(match.winner + ' wins!')
+        # print('Match ends in ' + str(match.time_elapsed) + ' seconds')
+        # print('Match ends in ' + str(match.counter_move) + ' moves')
+
+    print(f"White win rate: {(white_wins/SAMPLE_SIZE) * 100}%")
+    print(f"Black win rate: {(black_wins/SAMPLE_SIZE) * 100}%")
+    print(f"Draw rate: {(draws/SAMPLE_SIZE) * 100}%")
+
+
+if __name__ == '__main__':
+    main()
--- group1.py
+++ group1.py
@@ -2,7 +2,9 @@
 
 from game.go import Board
 import random
-
+from game.go import opponent_color
+from game.util import PointDict
+from copy import deepcopy
 
 class Agent1:
     """A class to generate a random action for a Go board."""
@@ -20,4 +22,162 @@ class Agent1:
         actions = board.get_legal_actions()
         if actions:
             return random.choice(actions)
+        return None
+    
+class BoardSnapshot:
+    def __init__(self, board: Board):
+        # simple scalars
+        self.next = board.next
+        self.winner = board.winner
+        self.end_by_no_legal_actions = board.end_by_no_legal_actions
+        self.counter_move = board.counter_move
+        self.legal_actions = board.get_legal_actions()
+
+        group_mapping = {group: deepcopy(group) for group in board.groups['BLACK'] + board.groups['WHITE']}
+
+        # groups & lists – just keep references; they’re immutable for our backtracking step
+        self.groups = {
+            'BLACK': [group_mapping[group] for group in board.groups['BLACK']],
+            'WHITE': [group_mapping[group] for group in board.groups['WHITE']]
+        }
+        self.endangered = [group_mapping[group] for group in board.endangered_groups]
+        self.removed = [group_mapping[group] for group in board.removed_groups]
+
+        # PointDict shallow snapshot: dict of dict of lists
+        self.stonedict = {
+            'BLACK': {p: list(group_mapping[group] for group in groups) for p, groups in board.stonedict.d['BLACK'].items()},
+            'WHITE': {p: list(group_mapping[group] for group in groups) for p, groups in board.stonedict.d['WHITE'].items()}
+        }
+        self.libertydict = {
+            'BLACK': {p: list(group_mapping[group] for group in groups) for p, groups in board.libertydict.d['BLACK'].items()},
+            'WHITE': {p: list(group_mapping[group] for group in groups) for p, groups in board.libertydict.d['WHITE'].items()}
+        }
+
+def restore(board: Board, snap: BoardSnapshot):
+    board.next = snap.next
+    board.winner = snap.winner
+    board.end_by_no_legal_actions = snap.end_by_no_legal_actions
+    board.counter_move = snap.counter_move
+    board.groups['BLACK'] = list(snap.groups['BLACK'])
+    board.groups['WHITE'] = list(snap.groups['WHITE'])
+    board.endangered_groups = list(snap.endangered)
+    board.removed_groups = list(snap.removed)
+    board.legal_actions = snap.legal_actions
+
+    # restore PointDicts
+    board.stonedict.d['BLACK'] = {p: list(groups) for p, groups in snap.stonedict['BLACK'].items()}
+    board.stonedict.d['WHITE'] = {p: list(groups) for p, groups in snap.stonedict['WHITE'].items()}
+    board.libertydict.d['BLACK'] = {p: list(groups) for p, groups in snap.libertydict['BLACK'].items()}
+    board.libertydict.d['WHITE'] = {p: list(groups) for p, groups in snap.libertydict['WHITE'].items()}
+
+class Agent1v1:
+    """A class to generate a random action for a Go board."""
+    def __init__(self, color):
+        self.color = color
+        self.MAX_DEPTH = 3
+
+        self.W_LIBERTIES = 10
+        self.W_ATARI = 40
+        self.W_WIN = 1e5
+
+    def evaluate(self, board: Board) -> int:
+        if(board.winner is not None):
+            if(board.winner == self.color): return self.W_WIN
+            else: return -self.W_WIN
+        score = 0
+        score += self.W_LIBERTIES * (len(board.libertydict.d[self.color]) - len(board.libertydict.d[opponent_color(self.color)]))
+        return score
+
+    def minimax(self, board: Board, depth: int, maximizing_player: bool) -> int:  
+        pos = 0   
+        if((depth == 0) or (board.winner is not None)):
+            return self.evaluate(board),1
+
+        if(len(board.get_legal_actions()) == 0): 
+            return 0,1
+
+        if(maximizing_player):
+            max_score = -1e9
+            for action in board.get_legal_actions():
+                # snap = BoardSnapshot(board)
+                # board.put_stone(action, check_legal=False)
+                successor = board.copy()
+                successor.put_stone(action, check_legal=False)
+                score, new_pos = self.minimax(successor, depth-1, False)
+                # score, new_pos = self.minimax(board, depth-1, False)
+                max_score = max(max_score, score)
+                pos += new_pos
+                # restore(board, snap)
+            return max_score, pos
+        
+        else:
+            min_score = 1e9
+            for action in board.get_legal_actions():
+                # snap = BoardSnapshot(board)
+                # board.put_stone(action, check_legal=False)
+                successor = board.copy()
+                successor.put_stone(action, check_legal=False)
+                score, new_pos = self.minimax(successor, depth-1, True)
+                # score, new_pos = self.minimax(board, depth-1, True)
+                min_score = min(min_score, score)
+                pos += new_pos
+                # restore(board, snap)
+            return min_score, pos
+
+    def get_best_action(self, board: Board, depth: int):
+        pos = 0
+        best_score = -1e9
+        best_action = None
+        # self.print_point_dict(board.libertydict)
+        # print(f"Legal Actions: {board.get_legal_actions()}")
+        for action in board.get_legal_actions():
+            snap = BoardSnapshot(board)
+            
+            # print("STORED_______________________")
+            # for key,val in snap.libertydict['BLACK'].items(): 
+            #     print(key , val)
+        
+            # for key,val in snap.libertydict['WHITE'].items(): 
+            #     print(key , val)
+            # print()
+
+            board.put_stone(action, check_legal=False)
+            # successor = board.copy()
+            # successor.put_stone(action, check_legal=False)
+            # score, new_pos = self.minimax(successor, depth, maximizing_player=False)
+            # self.print_point_dict(board.libertydict)
+            score, new_pos = self.minimax(board, depth, maximizing_player=False)
+            pos += new_pos
+            if(score > best_score):
+                best_score = score
+                best_action = action
+            
+            restore(board, snap)
+            # print("RESTORED: ")
+            # self.print_point_dict(board.libertydict)
+
+        print(f"Possibilities considered: {pos}")
+        return best_action
+            
+    def print_point_dict(self, point_dict:PointDict):
+        for key,val in point_dict.d['BLACK'].items(): 
+            print(key , val)
+        
+        for key,val in point_dict.d['WHITE'].items(): 
+            print(key , val)
+        print()
+
+    def get_action(self, board: Board):
+        """
+        Returns a random legal action from the board.
+
+        :param board: The current Go board state.
+        :return: A random legal action (tuple) or None if no actions are available.
+        """
+
+        actions = board.get_legal_actions()
+        if actions:
+            best_action = self.get_best_action(board, self.MAX_DEPTH)
+            # print("BEST ACTION: ", best_action, board.next)
+            return best_action
         return None
